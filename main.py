7184de4d4d0d72ef529925563fc7f50fd2ab54f6
Timotimyp <60345296+Timotimyp@users.noreply.github.com>
2022-03-27 13:37:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if count == 0:
     # который отправила нам Алиса в запросе POST
     def main():
         logging.info(f'Request: {request.json!r}')
-    
+
         # Начинаем формировать ответ, согласно документации
         # мы собираем словарь, который потом при помощи
         # библиотеки json преобразуем в JSON и отдадим Алисе
@@ -55,26 +55,26 @@ if count == 0:
                 'end_session': False
             }
         }
-    
+
         # Отправляем request.json и response в функцию handle_dialog.
         # Она сформирует оставшиеся поля JSON, которые отвечают
         # непосредственно за ведение диалога
         handle_dialog(request.json, response)
-    
+
         logging.info(f'Response:  {response!r}')
-    
+
         # Преобразовываем в JSON и возвращаем
         return json.dumps(response)
-    
-    
-    def handle_dialog(req, res):
+
+
+    def handle_dialog(req, res, count):
         user_id = req['session']['user_id']
-    
+
         if req['session']['new']:
             # Это новый пользователь.
             # Инициализируем сессию и поприветствуем его.
             # Запишем подсказки, которые мы ему покажем в первый раз
-    
+
             sessionStorage[user_id] = {
                 'suggests': [
                     "Не хочу.",
@@ -87,7 +87,7 @@ if count == 0:
             # Получим подсказки
             res['response']['buttons'] = get_suggests(user_id)
             return
-    
+
         # Сюда дойдем только, если пользователь не новый,
         # и разговор с Алисой уже был начат
         # Обрабатываем ответ пользователя.
@@ -107,28 +107,28 @@ if count == 0:
             # Пользователь согласился, прощаемся.
             res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
             res['response']['end_session'] = True
-            return
-    
+            count += 1
+
         # Если нет, то убеждаем его купить слона!
         res['response']['text'] = \
             f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
         res['response']['buttons'] = get_suggests(user_id)
-    
-    
+
+
     # Функция возвращает две подсказки для ответа.
     def get_suggests(user_id):
         session = sessionStorage[user_id]
-    
+
         # Выбираем две первые подсказки из массива.
         suggests = [
             {'title': suggest, 'hide': True}
             for suggest in session['suggests'][:2]
         ]
-    
+
         # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
         session['suggests'] = session['suggests'][1:]
         sessionStorage[user_id] = session
-    
+
         # Если осталась только одна подсказка, предлагаем подсказку
         # со ссылкой на Яндекс.Маркет.
         if len(suggests) < 2:
@@ -137,9 +137,8 @@ if count == 0:
                 "url": "https://market.yandex.ru/search?text=слон",
                 "hide": True
             })
-    
+
         return suggests
-    count += 1
 else:
     @app.route('/post', methods=['POST'])
     # Функция получает тело запроса и возвращает ответ.
